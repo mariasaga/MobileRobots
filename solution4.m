2dffe0ee8c59132bc722897e3bd3f113233dcdec
saadejazz <60652466+saadejazz@users.noreply.github.com>
2022-05-19 14:57:26 +0200

    Update solution4.m

--- solution4.m
+++ solution4.m
@@ -2,79 +2,76 @@ function [forwBackVel, leftRightVel, rotVel, finish] = solution4(pts, contacts, 
 
     % State Machine (FSM)
     persistent state;
-    if isempty(state),
+    if isempty(state)
         % the initial state of the FSM is 'init'
         state = 'init';
     end
+
+    dist = 1;
+    p_parallel = 2;
 
     % initialize the robot control variables (returned by this function)
     finish = false;
     forwBackVel = 0;
     leftRightVel = 0;
-    rotVel = 0;
+    rotVel = 0.1;
 
     % get the laser contact points in sensor's coordinates
-    points = [pts(1,contacts)' pts(2,contacts)'];
-   
+     points = [pts(1,contacts)' pts(2,contacts)'];
     % calculate the distances
-    distances = pts(1,contacts)'.^2 + pts(2,contacts)'.^2;
+    distances = (pts(1,contacts)'.^2 + pts(2,contacts)'.^2).^0.5;
     % get the closest point
-    [min_value, min_index] = min(distances);
+    [min_value, min_index] = min(distances(:));
 
     % manage the states of FSM
     if strcmp(state, 'init')
-        state = 'move_forward';
-        fprintf('changing FSM state to %s\n', state);
-        % save the initial position
-
-    elseif strcmp(state, 'move_forward')
-        % move straight
         forwBackVel = -3;
         leftRightVel = 0;
         rotVel = 0;
-
-        % when close to a wall change state and move left
-        if min_value <= 1
-            state = 'left_move';
-            fprintf('changing FSM state to %s\n', state);
+        
+        if min_value < dist
+            state = 'move';
         end
 
-    elseif strcmp(state, 'left_move')
+    elseif strcmp(state, 'move')
 
         % calculate vector to min distance
+        phi = orientation(3);
         vec_wall = points(min_index, :);
+        x = cos(phi) * vec_wall(1) - sin(phi) * vec_wall(2);
+        y = sin(phi) * vec_wall(1) + cos(phi) * vec_wall(2);
+        vec_wall = [x y];
 
         % calculate perpendicular vector for movement
         if vec_wall(1) == 0 
-            vec_per = [1 0];
+            vec = [1 0];
         else
-            vec_per = [-(vec_wall(2)/vec_wall(1)) 1];
-            vec_per = 3* vec_per/norm(vec_per);
+            vec = [-vec_wall(2)/vec_wall(1) 1];
+            vec = vec/norm(vec);
         end 
 
-        % regulator of the distance
+        speed_x = cos(phi) * vec(1) + sin(phi) * vec(2);
+
+        if speed_x < 0
+            vec = -vec;
+        end
         
+        
+        % distance regulator perp
+        (norm(vec_wall) - dist) * vec_wall;
+
+
+        vec = P * dist * vec;
+
         goal_orient = atan2(vec_wall(1), -vec_wall(2));
+        rotVel = 10 * (goal_orient - phi);
 
-        phi = orientation(3);
-        rotVel = 5 * (goal_orient - phi);
-
-        speed_x = cos(phi) * vec_per(1) + sin(phi) * vec_per(2);
-        speed_y = -sin(phi) * vec_per(1) + cos(phi) * vec_per(2);
-
-        % move left
+        speed_x = cos(phi) * vec(1) + sin(phi) * vec(2);
+        speed_y = -sin(phi) * vec(1) + cos(phi) * vec(2);
+        
         forwBackVel = speed_y;
         leftRightVel = speed_x;
-%         rotVel = 0;
 
-        
-        % when there is more than 1 point equally close NOT WORKING 
-        if length(min_index)>=2
-            % rotate to mantain perpendicular to the wall
-            fprintf('2 same points reached :)');
-            % continue advancing new left
-        end
-        fprintf('changing FSM state to %s\n', state);
     else
         error('Unknown state %s.\n', state);
     end
