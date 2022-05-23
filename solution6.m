50002091f30a2429cd01ebf4508facb25c5fadc6
mariasaga <103196309+mariasaga@users.noreply.github.com>
2022-05-23 13:44:17 +0200

    update

--- solution6.m
+++ solution6.m
@@ -30,8 +30,19 @@ function [forwBackVel, leftRightVel, rotVel, finish] = solution5(pts, contacts, 
 
         % Path Planning 
         
-        % check map exists 
-        
+        % check map exists
+        Directory = fullfile ('C:','emor','ws_emor','emor_trs','youbot', 'vrep_env', map2);
+        Images = dir(fullfile(Directory,'*.png'));
+        Names = {Images.name}';
+        for k=1: numel(Names)
+               try
+                I=imread(fullfile(Directory,Names{k})); 
+              catch
+                error( ...)
+                end
+               end
+        end
+
         % read the map 
         map = imread('vrep_env/map2.png');  % we have to add the .png? check if it exists
 
@@ -46,13 +57,14 @@ function [forwBackVel, leftRightVel, rotVel, finish] = solution5(pts, contacts, 
         y_init = position(2);
 
         % units conversion from world to image
-        x_init_map=round( 100*((x_init-(-7.5))/(7.5-(-7.5))));
-        y_init_map=round( 200*((y_init-(-7.5))/(7.5-(-7.5))));
+        x_init_map=round( size_x*((x_init-(-7.5))/(7.5-(-7.5))));
+        y_init_map=round( size_y*((y_init-(-7.5))/(7.5-(-7.5))));
 
         x_goal_map = round( 100*((goal_x-(-7.5))/(7.5-(-7.5))));
         y_goal_map = round( 100*((goal_y-(-7.5))/(7.5-(-7.5))));
 
 
+        % Wavefront Planner - Phase 1
         for i = 1: size_x
             for j = 1: size_y
                 % if the cell is free
@@ -64,10 +76,55 @@ function [forwBackVel, leftRightVel, rotVel, finish] = solution5(pts, contacts, 
                 % if the cell is the goal cell put a 2
                 elseif i == x_goal_map && j == y_goal_map
                     matrix(j,i) = 2;
+                % if unclassified, obstacle
+                else 
+                    matrix(j,i) = 1; 
+                end
+            end
+        end
+        
+        find = 2;       % find value to change neighbor cell values
+
+        % while the initial position has not been reasigned a value
+        while matrix (y_init_map, x_init_map) == 0
+            for i = 1: size_x
+                for j = 1: size_y
+                    % check if we are in the found value
+                     if matrix(j,i) == find
+
+                        % check not a corner
+
+                        % check neighbors not find
+
+                        matrix(j+1,i) = find + 1;
+                        matrix(j-1, i) = find + 1;
+                        matrix(j, i+1) = find + 1;
+                        matrix(j, i-1) = find + 1;
+                        matrix(j+1, i+1) = find + 1;
+                        matrix(j+1, i-1) = find + 1;
+                        matrix(j-1, i+1) = find + 1;
+                        matrix(j-1, i-1) = find + 1;
+
+
+                        find = find + 1; % update value to find
+                     end
+
                 end
             end
         end
 
+        % Wavefront Planner - Phase 2
+        for i = 1: size_x
+            for j = 1: size_y
+                
+
+            end
+        end
+
+
+        % units conversion from IMAGE to WORLD
+        x_world = x_image *(7.5-(-7.5))/size_x + (-7.5);
+        y_world = y_image *(7.5-(-7.5))/size_y + (-7.5);
         
 
     elseif strcmp(state, 'move')
